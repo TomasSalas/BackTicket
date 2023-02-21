20eda099da928c6ea64c8b9774f303e32a4ad1d1
Tomas Salas Arancibia <57121296+TomasSalas@users.noreply.github.com>
2023-02-21 12:53:16 -0300

    fix-console-log

--- Controllers/Ticket/updateStateTicket.js
+++ Controllers/Ticket/updateStateTicket.js
@@ -1,6 +1,7 @@
 import { pool } from "../../Config/index.js";
 
 export const updateStateTicket = async (req, res) => {
+  console.log('reqBody' , req.body)
   const { idTicket , Estado} = req.body
   const [rows] = 
   await pool.query(`UPDATE TICKET set ID_ESTADO = ? WHERE ID_TICKET = ?` , [Estado , idTicket])
