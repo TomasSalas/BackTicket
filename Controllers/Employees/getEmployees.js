f28445b65729f42f2dc65eddecdcde3421b36b0d
Tomas Salas Arancibia <57121296+TomasSalas@users.noreply.github.com>
2023-02-21 12:43:26 -0300

    fix-name-table

--- Controllers/Employees/getEmployees.js
+++ Controllers/Employees/getEmployees.js
@@ -4,8 +4,8 @@ export const getEmployees = async (req, res) => {
   const [rows] = await pool.query(
     `SELECT 	
       E.ID_EMPLEADO , E.NOMBRES , E.APELLIDOS , E.CORREO  , E.TELEFONO , E.PASSWORD , C.NOMBRE as 'CARGO' , ES.NOMBRE  as 'ESTADO' FROM Empleado E 
-      JOIN Estado ES on ES.ID_ESTADO  = E.ID_ESTADO 
-      JOIN Cargo C on C.ID_CARGO = E.ID_CARGO `
+      JOIN ESTADO ES on ES.ID_ESTADO  = E.ID_ESTADO 
+      JOIN CARGO C on C.ID_CARGO = E.ID_CARGO `
     );
   res.json({ Empleado: rows });
 }
