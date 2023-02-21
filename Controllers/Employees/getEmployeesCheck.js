90a773a864a3eeed2dcf889f6bc1e10c0714d97e
Tomas Salas Arancibia <57121296+TomasSalas@users.noreply.github.com>
2023-02-21 13:14:53 -0300

    fix-employees-check

--- Controllers/Employees/getEmployeesCheck.js
+++ Controllers/Employees/getEmployeesCheck.js
@@ -5,8 +5,8 @@ export const getEmployeesCheck = async (req, res) => {
     `SELECT 	
     E.ID_EMPLEADO , CONCAT(E.NOMBRES , ' ' ,E.APELLIDOS) AS NOMBRE
     FROM Empleado E 
-    JOIN Estado ES on ES.ID_ESTADO  = E.ID_ESTADO 
-    JOIN Cargo C on C.ID_CARGO = E.ID_CARGO
+    JOIN ESTADO ES on ES.ID_ESTADO  = E.ID_ESTADO 
+    JOIN CARGO C on C.ID_CARGO = E.ID_CARGO
     WHERE E.ID_ESTADO = '1'
     `
   );
