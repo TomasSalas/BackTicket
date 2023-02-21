ae54de4479a8d91f182843050f33fc1083bbe082
Tomas Salas Arancibia <57121296+TomasSalas@users.noreply.github.com>
2023-02-21 13:25:48 -0300

    add-cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import * as dotenv from 'dotenv'
 dotenv.config()
 import routes from './Routes/index.js'
-
+import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 1313
 
+app.use(cors())
 app.use(routes)
 app.listen(port , () => {}) 
